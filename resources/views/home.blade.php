02b25954490c29824e8c71b99b2231693ae80381
Devin de Spindler <devin.vipxxx@gmail.com>
2025-01-09 21:22:19 +0100

    add: Layout with HTML and Tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,67 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+    <div class="mt-12"></div>
+    <!-- Grid mit farbigen Kästchen -->
+    <div class="grid grid-cols-5 gap-3 w-11/12 mx-auto">
+        <div class="bg-gray-300 px-3 py-2">
+            <a class="text-2xl">Anlagen<br></a>
 
-    <h1 class="title">Welcome</h1>
+            <div class="grid grid-cols-2">
+                <a>aktiviert<br></a>
+                <a class="justify-self-end">53</a>
+
+                <a>deaktiviert</a>
+                <a class="justify-self-end">1</a>
+            </div>
+        </div>
+        <div class="bg-red-200 px-3 py-2">
+            <a class="text-2xl">Notrufe<br></a>
+
+            <div class="grid grid-cols-2">
+                <a>eingehende Anrufe<br></a>
+                <a class="justify-self-end">0</a>
+
+                <a>aktive Notrufe</a>
+                <a class="justify-self-end">6</a>
+            </div>
+        </div>
+        <div class="bg-blue-200 px-3 py-2">
+            <a class="text-2xl">Überfällig<br></a>
+
+            <div class="grid grid-cols-2">
+                <a>Routinerufe<br></a>
+                <a class="justify-self-end">19</a>
+
+                <a>lokale Kontrollen</a>
+                <a class="justify-self-end">3</a>
+            </div>
+        </div>
+        <div class="bg-orange-200 px-3 py-2">
+            <a class="text-2xl">Alarme<br></a>
+
+            <div class="grid grid-cols-2">
+                <a>kritisch<br></a>
+                <a class="justify-self-end">29</a>
+
+                <a>unkritisch</a>
+                <a class="justify-self-end">5</a>
+            </div>
+        </div>
+        <div class="bg-green-200 px-3 py-2">
+            <a class="text-2xl">Service Level<br></a>
+
+            <div class="grid grid-cols-2">
+                <a>Routinerufe<br></a>
+                <a class="justify-self-end">9%</a>
+
+                <a>lokale Kontrollen</a>
+                <a class="justify-self-end">85%</a>
+            </div>
+
+        </div>
+        
+    </div>
     
 </body>
 </html>
